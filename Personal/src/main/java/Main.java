9fc183637b997dfdb11ae8fdc9b8f0671e5e063d
Yanyu07 <gxp43999378@outlook.com>
2023-11-01 21:47:40 +0800

    变更

---
+++ Personal/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {9,1,3,4,6,8,2,7,3,6,4};
+
+        System.out.println(Arrays.toString(SelectionSort.selectionSort(arr)));
+    }
+
+}
---
+++ Personal/src/main/java/SelectionSort.java
@@ -0,0 +1,25 @@
+import com.sun.org.apache.bcel.internal.generic.SWAP;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static int[] selectionSort(int[] arr){
+
+        if (arr == null || arr.length < 2) return arr;
+
+        for (int i = 0;i < arr.length ;i++){
+            int minIndex = i;
+            for (int j = i+1;j < arr.length ;j++){
+                minIndex = arr[j] < arr[minIndex]? j : minIndex;
+            }
+            swap(arr,i,minIndex);
+        }
+        return arr;
+    }
+
+    public static void swap(int[] arr,int i,int j){
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+}
